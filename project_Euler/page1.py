3df9668614832d9a8c349340769fccec034d1417
Hong JW <sighingtree@naver.com>
2018-02-06 14:10:18 +0000

    until Q9

--- project_Euler/page1.py
+++ project_Euler/page1.py
@@ -1,3 +1,7 @@
+#############################3#Problem1
+
+
+
 #Multiples of 3 and 5
 def mul():
     result = 0
@@ -6,7 +10,16 @@ def mul():
             result += i
     return result
 
-#problem2
+
+
+
+
+
+######################################problem2
+
+
+
+
 #get fibonacci and plus the number which is even
 def fibonacci():
     flist = [1,2]
@@ -20,7 +33,14 @@ def fibonacci():
 
 
 
-#problem3
+
+
+
+
+
+##########################problem3
+
+
 
 def isPrime(num):
     retval = False
@@ -40,6 +60,8 @@ def isPrime(num):
                 continue
     return retval
 
+
+
 def getPrimeNum(num):
     dlist = []
     dlist2 = []
@@ -52,7 +74,7 @@ def getPrimeNum(num):
         else:
             continue
     dlist.sort()
-    
+    print(dlist)
     for j in dlist:
         boolval = isPrime(j)
         if boolval == True:
@@ -63,7 +85,13 @@ def getPrimeNum(num):
     return largest
 
 
-#Problem4
+
+
+########################Problem4
+
+
+
+
 #Making palindrome
 
 def makePal(tranNum):
@@ -130,7 +158,15 @@ def getBigPal(digP):
 
 #print(getBigPal(3))
 
-#Question 5
+
+
+
+
+
+#################Question 5
+
+
+
 #2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
@@ -179,6 +215,181 @@ def getNum(ran):
 
 #print(getNum(30))
 
+#Question6
+
+
+sqrsSum = 0
+sumSqr = 0
+for i in range(1,101):
+    sqrsSum += i ** 2
+
+for i in range(1,101):
+    sumSqr += i
+
+#result = (sumSqr ** 2) - sqrsSum
+#print(result)
 
 
 
+
+
+
+
+
+################Question7
+
+
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+#What is the 10 001st prime number?
+
+class getTargetP:
+    def __init__(self):
+        self.targetNo = 2
+        self.intervNum = 3
+    
+    def getPr(self):
+        while self.targetNo <= 10001:
+            self.intervNum += 2
+            if isPrime(self.intervNum):
+                self.targetNo += 1
+                if self.targetNo == 10001:
+                    print(self.intervNum)
+            else:   
+                continue
+    
+        
+        
+        
+        
+##################Question8
+
+
+
+numbers = '''7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450'''
+
+
+targetList = [0]
+
+for i in range(0,len(numbers)-13):
+    for j in range(i,i+13):
+        if numbers[j] ==0:
+            i += 1
+
+    targetNum = 1
+    for j in range(i,i+13):
+        targetNum *= int(numbers[j])
+    targetList.append(targetNum)
+    
+        
+    if targetList[0] <= targetList[1]:
+        del targetList[0]
+    else:
+        targetList.pop()
+        
+#print(targetList.pop())
+
+
+
+#another way to solve
+
+from string import whitespace
+
+class anotherSolve:
+    def __init__(self):
+        self.numFile = open('numbers.txt','r')
+        self.numbers2 = [int(nums) for lines in self.numFile for nums in lines if nums not in whitespace]
+        
+    def targetNum(self, n):
+        numList = self.numbers2
+        targetList = [0]
+        targetNum2 = 1
+        for i in range(0, len(numList)-n):
+            for j in range(i, i+n):
+                if numList[j] == 0:
+                    break
+                targetNum2 *= numList[j]
+            targetList.append(targetNum2)
+            
+            if targetList[0] > targetList[1]:
+                targetList.pop()
+                
+            else:
+                del targetList[0]
+                
+            targetNum2 = 1
+        return targetList.pop()
+
+
+#test = anotherSolve()
+#print(test.targetNum(13))
+                
+                
+                
+
+
+##################################Question9
+
+
+
+
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+#a2 + b2 = c2
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
+
+
+
+
+#Because they say there exactly exist 1000 that meet the condition...
+#It means that we can use aliquot of 1000 like under this ex
+#(25*a)^2 + (25*b)^2 = (25^c)^2
+
+
+
+#Get the aliquotes of 1000
+
+def getAliquot(num):
+    sqrtNum = int(num ** 0.5)
+    alList = []
+    for i in range(1, sqrtNum):
+        if num%i == 0:
+            if i != 1:
+                alList.append(i)
+        else:
+            continue
+        i += 1
+    alList.sort()
+    return alList
+    
+def findTriplet(QNum):
+    divide = 0
+    alList = getAliquot(QNum)
+    retNum = 0
+    
+    for i in alList:
+        divide = QNum / i
+        a, b = 1, 2
+        c = divide - a - b
+        
+        while a>0 and b>0 and c >0 and b >= a:
+            if (a ** 2) + (b ** 2) == (c ** 2):
+                retNum = a*i*b*i*c*i
+                print(a, b, c ,i)
+                return retNum
+            else:
+                if c <=  b:
+                    a += 1
+                    b = a+1
+                    c = divide - a - b
+                else:
+                    b += 1
+                    c -= 1
+                    
+            
+        
+print(findTriplet(1000))
